336091fbca0bc19411394f4bda485b4c94e401a4
Trey Hakanson <treyhakanson@gmail.com>
2019-07-02 22:53:45 -0400

    🎮 Unity dump

---
+++ Unity/Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard: MonoBehaviour {
+	// C# is statically typed
+	int max = 1000; // these values are globally scoped to the entire instance
+	int min = 1;
+
+	public Text title;
+	public Text subtitle;
+	public Text response;
+
+	// Use this for initialization
+	void Start () {
+		// like swift, C# requires string literals to be in
+		// double quotes. Single quotes are reserved for
+		// character literals
+		print("Welcome to Number Wizard!");
+		print("Pick a number in your head, but don\'t tell me!");
+
+		// interpolated strings are preceded by a $, and
+		// use curly braces for the variable names
+		print($"the largest number you can pick is {max}");
+		print($"and the lowest number you can pick is {min}");
+
+		// this just handles the edge case of 1000; without
+		// this, the max integer that will be guessed is 999
+		// (due to rounding)
+		this.max++;
+
+		this.AskQuestion();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			this.min = this.ComputeGuess();
+			this.AskQuestion();
+		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			this.max = this.ComputeGuess();
+			this.AskQuestion();
+		} else if (Input.GetKeyDown(KeyCode.Return)) {
+			print("I won!");
+		}
+	}
+
+	int ComputeGuess() {
+		return (this.min + this.max)/2;
+	}
+
+	void AskQuestion() {
+		print($"Is the number higher or lower than {this.ComputeGuess()}");
+		print("[Up] Higher, [Down] Lower, [Enter] equal"); 
+	}
+}
---
+++ Unity/Text101/Assets/Scripts/TextController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI; // brings in unity UI elements
+
+public class TextController : MonoBehaviour {
+	// this makes a text variable of type Text availble to the entire program.
+	// This will later be connected to the Text UI element created in Unity
+	public Text text;
+	private enum StoryState { cell, mirror, lock_0, sheets_0, cell_mirror, sheets_1, lock_1, freedom };
+	private StoryState storyState;
+
+	// Use this for initialization
+	void Start () {
+		storyState = StoryState.cell;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (storyState) {
+			case StoryState.cell: 			
+				state_cell();
+				break;
+			case StoryState.mirror:
+				state_cell();
+				break;
+			case StoryState.sheets_0:
+				state_sheets_0();
+				break;
+			case StoryState.lock_0:
+				state_cell();
+				break;
+			case StoryState.cell_mirror:
+				state_cell();
+				break;
+			case StoryState.sheets_1:
+				state_cell();
+				break;
+			case StoryState.lock_1:
+				state_cell();
+				break;
+			case StoryState.freedom:
+				state_cell();
+				break;
+		}
+	}
+
+	void state_sheets_0() {
+		text.text = "State - Sheets 0 (Press R to Return to Cell)";
+
+		if (Input.GetKeyDown(KeyCode.R))
+			storyState = StoryState.cell;
+	}
+
+	void state_cell() {
+		text.text = "State - Cell (Press S to Look at Sheets)";
+		
+		if (Input.GetKeyDown(KeyCode.S))
+			storyState = StoryState.sheets_0;
+	}
+}
